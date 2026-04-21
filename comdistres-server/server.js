d5a0fa178548c67eecdde0dea6fbb8418f75f2f5
shadowabu254 <shadowabu@gmail.com>
2026-04-21 21:49:06 +0300

    fixed origin issue

--- comdistres-server/server.js
+++ comdistres-server/server.js
@@ -57,7 +57,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_ORIGIN || "http://localhost:3000",
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:3000" || "https://desastersystem.onrender.com",
     methods: ["GET", "POST"],
     credentials: true,
   },

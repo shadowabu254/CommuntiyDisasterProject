6b1f49c327822263f5ec90486105ad071b7aeab6
shadowabu254 <shadowabu@gmail.com>
2026-04-21 22:08:12 +0300

    signup api fixed

--- comdistres-client/src/context/AuthContext.js
+++ comdistres-client/src/context/AuthContext.js
@@ -14,7 +14,7 @@ export function AuthProvider({ children }) {
   useEffect(() => {
     const fetchMe = async () => {
       try {
-        const res = await api.get('/auth/me');
+        const res = await api.get('https://disaster-system-l3zu.onrender.com/auth/me');
         setUser(res.data);
       } catch (err) {
         setUser(null);

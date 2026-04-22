bfe09c9971899cd44bacae9b854b54c43a25fb4a
shadowabu254 <shadowabu@gmail.com>
2026-04-22 10:37:15 +0300

    .env update

--- comdistres-server/db.js
+++ comdistres-server/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS || "",
+  process.env.DB_PASS,
   {
     host:    process.env.DB_HOST,
     port:    parseInt(process.env.DB_PORT) || 3306,

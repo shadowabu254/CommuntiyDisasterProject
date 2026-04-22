e9e3b4396970f35d9ecddad1d006b4e88840300b
shadowabu254 <shadowabu@gmail.com>
2026-04-22 17:15:33 +0300

    updated axios.js

--- comdistres-client/src/api/axios.js
+++ comdistres-client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://disaster-system-l3zu.onrender.com/api',
+  baseURL:'https://disaster-system-l3zu.onrender.com/api',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' }
 });
